6312109d093df7ff088d80bf082ae2bf97407910
DiegoDev <144474823+CodeDiego15@users.noreply.github.com>
2024-06-04 04:10:29 -0400

    commit

--- mac_changer.py
+++ mac_changer.py
@@ -9,31 +9,27 @@ __version__ = "0.1.0"
 def print_banner():
     # Import colorama within the function
 
-    print(Fore.LIGHTRED_EX + """
-          
-            
-    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⢿⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⠇
-⠀⠈⢿⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⡷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⠏⠀
-⠀⠀⠈⢻⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣤⣤⣶⣶⣶⣾⣿⣿⣿⣿⣿⣶⣶⣶⣦⣤⣄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⠏⠀⠀
-⠀⠀⠀⠀⢻⣿⣄⠀⠀⠀⠀⣀⣤⣶⣿⡿⢟⣿⣿⡿⠛⠋⠉⠀⠀⠀⠀⠈⠉⠛⠻⣿⣿⣿⠿⣿⣷⣦⣄⡀⠀⠀⠀⠀⣼⣿⠃⠀⠀⠀
-⠀⠀⠀⠀⠀⠛⠋⠀⣠⣶⣿⠿⠛⠉⢀⣴⣿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣷⣄⠈⠙⠻⢿⣷⣦⡀⠈⠛⠃⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⢀⣴⣿⠿⠋⠁⠀⠀⢠⣿⡟⠁⠀⠀⠀⠀⠀⠀⢀⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠙⣿⣧⠀⠀⠀⠉⠻⢿⣷⣄⠀⠀⠀⠀⠀
-⠀⠀⠀⣠⣾⣿⠟⠁⠀⠀⠀⠀⢠⣿⡟⠀⠀⠀⠀⠀⢀⣴⣿⠿⠿⠿⠿⢿⣷⣄⠀⠀⠀⠀⠀⠘⣿⣧⠀⠀⠀⠀⠀⠙⢿⣿⣦⡀⠀⠀
-⠀⢀⣴⣿⠟⠁⠀⠀⠀⠀⠀⠀⣾⣿⠀⠀⠀⠀⠀⣰⣿⠟⠁⠀⠀⠀⠀⠀⠙⢿⣷⡀⠀⠀⠀⠀⢸⣿⡆⠀⠀⠀⠀⠀⠀⠘⢿⣿⣄⠀
-⣠⣿⡟⠁⠀⠀⠀⠀⠀⠀⠀⠀⣿⡇⠀⠀⠀⠀⠀⣿⡟⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⡇⠀⠀⠀⠀⠀⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣧
-⠻⣿⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⣿⡇⠀⠀⠀⠀⠀⣿⣇⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⡇⠀⠀⠀⠀⠀⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⡿
-⠀⠘⢿⣷⣄⠀⠀⠀⠀⠀⠀⠀⢿⣿⠀⠀⠀⠀⠀⠹⣿⣆⠀⠀⠀⠀⠀⠀⢀⣾⡿⠁⠀⠀⠀⠀⢸⣿⠇⠀⠀⠀⠀⠀⠀⢀⣴⣿⠟⠀
-⠀⠀⠀⠙⢿⣷⣄⡀⠀⠀⠀⠀⠘⣿⣇⠀⠀⠀⠀⠀⠙⠿⣿⣶⣤⣤⣴⣾⣿⠟⠁⠀⠀⠀⠀⢀⣿⡟⠀⠀⠀⠀⠀⢀⣴⣿⠟⠁⠀⠀
-⠀⠀⠀⠀⠀⠙⠿⣿⣦⣀⠀⠀⠀⠙⣿⣧⠀⠀⠀⠀⠀⠀⠀⠉⠉⠉⠉⠉⠀⠀⠀⠀⠀⠀⣠⣾⡟⠁⠀⠀⢀⣠⣾⡿⠟⠁⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠈⠛⢿⣿⣦⣄⡀⠘⢿⣷⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⠟⠀⣀⣤⣾⣿⠟⠋⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠻⢿⣿⣶⣽⣿⣿⣶⣤⣀⠀⠀⠀⠀⠀⠀⢀⣀⣤⣾⣿⣿⣵⣾⣿⠿⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠙⠛⠻⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠿⠛⠛⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                 
+    print(Fore.LIGHTBLUE_EX + """
+                    
+                    
 
-                    RedEye by DiegoDev. --help si necesita ayuda.
+ ██▀███  ▓█████ ▓█████▄ ▓█████ ▓██   ██▓▓█████ 
+▓██ ▒ ██▒▓█   ▀ ▒██▀ ██▌▓█   ▀  ▒██  ██▒▓█   ▀ 
+▓██ ░▄█ ▒▒███   ░██   █▌▒███     ▒██ ██░▒███   
+▒██▀▀█▄  ▒▓█  ▄ ░▓█▄   ▌▒▓█  ▄   ░ ▐██▓░▒▓█  ▄ 
+░██▓ ▒██▒░▒████▒░▒████▓ ░▒████▒  ░ ██▒▓░░▒████▒
+░ ▒▓ ░▒▓░░░ ▒░ ░ ▒▒▓  ▒ ░░ ▒░ ░   ██▒▒▒ ░░ ▒░ ░
+  ░▒ ░ ▒░ ░ ░  ░ ░ ▒  ▒  ░ ░  ░ ▓██ ░▒░  ░ ░  ░
+  ░░   ░    ░    ░ ░  ░    ░    ▒ ▒ ░░     ░   
+   ░        ░  ░   ░       ░  ░ ░ ░        ░  ░
+                 ░              ░ ░            
+                                                                https://github.com/CodeDiego15
+                                                                redEye by DiegoDev. --help si necesita ayuda.
+                                                                                                        
+
+
+                                                                        
+                                                                
 
     """ + Fore.RESET)
 
@@ -42,26 +38,20 @@ print_banner()
 
 
 # Define command-line arguments
-parser = ArgumentParser(description= "Change MAC address of a network interface")
+parser = ArgumentParser(description= Fore.GREEN + "Change MAC address of a network interface")
 parser.add_argument("-i", "--interface", dest="interface", required=True,
-                    help="Network interface to change its MAC address")
+                    help= Fore.GREEN + "Network interface to change its MAC address")
 parser.add_argument("-m", "--mac", dest="new_mac", required=True,
-                    help="New MAC address to set")
-parser.add_argument("-v", "--version", action="version", version=f"MACMorph {__version__}") 
+                    help= Fore.GREEN + "New MAC address to set")
+parser.add_argument("-v", "--version", action="version", version= Fore.GREEN + f"MACMorph {__version__}") 
 # Parse arguments
 args = parser.parse_args()
-while True:
-    if not args.interface:
-        args.interface = input(Fore.LIGHTRED_EX + "[-] Por favor, especifique una interfaz: ")
-    if not args.new_mac:
-        args.new_mac = input(Fore.LIGHTRED_EX + "[-] Por favor, especifique una nueva dirección MAC: ")
-    if args.interface and args.new_mac:
-        break
 
-#if not args.interface:
-    #parser.error(Fore.LIGHTRED_EX + "[-] Please specify an interface and a new MAC address")
-#elif not args.new_mac:
-    #parser.error(Fore.LIGHTRED_EX + "[-] Please specify an interface and a new MAC address")
+
+if not args.interface:
+    parser.error(Fore.LIGHTRED_EX + "[-] Please specify an interface and a new MAC address")
+elif not args.new_mac:
+    parser.error(Fore.LIGHTRED_EX + "[-] Please specify an interface and a new MAC address")
 
 
 interface = args.interface
@@ -69,7 +59,7 @@ new_mac = args.new_mac
 
 
 if interface and new_mac:
-    print(Fore.LIGHTCYAN_EX + f"[+] Changing MAC address {interface} to {new_mac}")
+    print(Fore.LIGHTCYAN_EX + f"{Fore.BLUE}╔═════${Fore.RESET} {Fore.LIGHTGREEN_EX} Changing MAC address {interface} to {new_mac}")
     subprocess.call(["ifconfig", interface, "up"])
     subprocess.call(["ifconfig", interface, "ether", new_mac])
 else:
@@ -80,7 +70,7 @@ ifconfig_result = subprocess.check_output(["ifconfig",interface])
 mac_address = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result.decode("utf-8"))
 
 if mac_address:
-    print(Fore.LIGHTGREEN_EX + f"[+] MAC address {mac_address.group(0)} changed successfully")
+    print(f"{Fore.BLUE}╔═════${Fore.RESET} {Fore.LIGHTGREEN_EX} MAC address {mac_address.group(0)} changed successfully")
 else:
     print(Fore.LIGHTRED_EX + "[-] MAC address not changed")
     
--- network_scanner.py
+++ network_scanner.py
@@ -1,15 +1,23 @@
 import scapy.all as scapy
-
-# network_scanner.py
+from colorama import Fore
 from scapy.all import srp, Ether, ARP, conf
+import json
+
+
+
 
 def scan(ip):
-    answered, unanswered = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=ip), timeout=2)
-    print(answered.summary())
-
-if __name__ == "__main__":
-    conf.verb = 0
-    scan("192.168.0.1/24")
-
-
-conf.verb = 0
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1)[0]
+    print(Fore.LIGHTBLUE_EX + "IP\t\t\tMAC Address\n----------------------------------------------------------")
+    client_list = []
+    
+    for element in answered_list:
+        client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+        client_list.append(client_dict)
+        print(Fore.LIGHTGREEN_EX + element[1].psrc + "\t\t" + element[1].hwsrc)
+        json.dump(client_list, open("ipMac.json", "w"))
+        client_list.sort(key=lambda y: sorted(y.keys()))
+scan("192.168.0.104/24")
