2732fd4f02710815189e09d184bbb6f36451f775
DiegoDev <144474823+CodeDiego15@users.noreply.github.com>
2024-06-05 02:11:51 -0400

    commit

--- mac_changer.py
+++ mac_changer.py
@@ -1,62 +1,48 @@
 #!/usr/bin/env python3
 # coding=utf-8
 import subprocess
-from argparse import ArgumentParser  # Use argparse instead of optparse
-from colorama import Fore 
+from argparse import ArgumentParser
+from colorama import Fore
 import re
+
+
 __version__ = "0.1.0"
 
-def print_banner():
-    # Import colorama within the function
 
+def print_banner():
     print(Fore.LIGHTBLUE_EX + """
                     
-                    
-
- ██▀███  ▓█████ ▓█████▄ ▓█████ ▓██   ██▓▓█████ 
-▓██ ▒ ██▒▓█   ▀ ▒██▀ ██▌▓█   ▀  ▒██  ██▒▓█   ▀ 
-▓██ ░▄█ ▒▒███   ░██   █▌▒███     ▒██ ██░▒███   
-▒██▀▀█▄  ▒▓█  ▄ ░▓█▄   ▌▒▓█  ▄   ░ ▐██▓░▒▓█  ▄ 
-░██▓ ▒██▒░▒████▒░▒████▓ ░▒████▒  ░ ██▒▓░░▒████▒
-░ ▒▓ ░▒▓░░░ ▒░ ░ ▒▒▓  ▒ ░░ ▒░ ░   ██▒▒▒ ░░ ▒░ ░
-  ░▒ ░ ▒░ ░ ░  ░ ░ ▒  ▒  ░ ░  ░ ▓██ ░▒░  ░ ░  ░
-  ░░   ░    ░    ░ ░  ░    ░    ▒ ▒ ░░     ░   
-   ░        ░  ░   ░       ░  ░ ░ ░        ░  ░
-                 ░              ░ ░            
-                                                                https://github.com/CodeDiego15
-                                                                redEye by DiegoDev. --help si necesita ayuda.
-                                                                                                        
-
-
-                                                                        
-                                                                
-
+   
+                      888                            
+                      888                            
+                      888                            
+888d888  .d88b.   .d88888  .d88b.  888  888  .d88b.  
+888P"   d8P  Y8b d88" 888 d8P  Y8b 888  888 d8P  Y8b 
+888     88888888 888  888 88888888 888  888 88888888 
+888     Y8b.     Y88b 888 Y8b.     Y88b 888 Y8b.         https://github.com/CodeDiego15 - redEye by DiegoDev. --help si necesita ayuda.
+888      "Y8888   "Y88888  "Y8888   "Y88888  "Y8888  
+                                        888          
+                                   Y8b d88P          
+                                    "Y88P"           
+                 
+            
+                                                
     """ + Fore.RESET)
 
 
 print_banner()
 
 
-# Define command-line arguments
-parser = ArgumentParser(description= Fore.GREEN + "Change MAC address of a network interface")
-parser.add_argument("-i", "--interface", dest="interface", required=True,
-                    help= Fore.GREEN + "Network interface to change its MAC address")
-parser.add_argument("-m", "--mac", dest="new_mac", required=True,
-                    help= Fore.GREEN + "New MAC address to set")
-parser.add_argument("-v", "--version", action="version", version= Fore.GREEN + f"MACMorph {__version__}") 
-# Parse arguments
+parser = ArgumentParser(description=Fore.GREEN + "Change MAC address of a network interface and scan network")
+parser.add_argument("-i", "--interface", dest="interface", required=True, help=Fore.GREEN + "Network interface to change its MAC address")
+parser.add_argument("-m", "--mac", dest="new_mac", required=True, help=Fore.GREEN + "New MAC address to set")
+parser.add_argument("-v", "--version", action="version", version=Fore.GREEN + f"MACMorph {__version__}")
+
 args = parser.parse_args()
-
-
-if not args.interface:
-    parser.error(Fore.LIGHTRED_EX + "[-] Please specify an interface and a new MAC address")
-elif not args.new_mac:
-    parser.error(Fore.LIGHTRED_EX + "[-] Please specify an interface and a new MAC address")
 
 
 interface = args.interface
 new_mac = args.new_mac
-
 
 if interface and new_mac:
     print(Fore.LIGHTCYAN_EX + f"{Fore.BLUE}╔═════${Fore.RESET} {Fore.LIGHTGREEN_EX} Changing MAC address {interface} to {new_mac}")
@@ -65,7 +51,7 @@ if interface and new_mac:
 else:
     print("Error: Unable to change MAC address")
 
-ifconfig_result = subprocess.check_output(["ifconfig",interface])
+ifconfig_result = subprocess.check_output(["ifconfig", interface])
 
 mac_address = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result.decode("utf-8"))
 
@@ -73,6 +59,6 @@ if mac_address:
     print(f"{Fore.BLUE}╔═════${Fore.RESET} {Fore.LIGHTGREEN_EX} MAC address {mac_address.group(0)} changed successfully")
 else:
     print(Fore.LIGHTRED_EX + "[-] MAC address not changed")
-    
-    
-    
+
+
+
--- network_scanner.py
+++ network_scanner.py
@@ -1,23 +1,44 @@
 import scapy.all as scapy
 from colorama import Fore
-from scapy.all import srp, Ether, ARP, conf
-import json
-
-
-
+import ipaddress
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    arp_request_broadcast = broadcast/arp_request
-    answered_list = scapy.srp(arp_request_broadcast, timeout=1)[0]
-    print(Fore.LIGHTBLUE_EX + "IP\t\t\tMAC Address\n----------------------------------------------------------")
+    arp_request_broadcast = broadcast / arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=0.1, verbose=False)[0]
+
     client_list = []
-    
+
     for element in answered_list:
         client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
         client_list.append(client_dict)
-        print(Fore.LIGHTGREEN_EX + element[1].psrc + "\t\t" + element[1].hwsrc)
-        json.dump(client_list, open("ipMac.json", "w"))
-        client_list.sort(key=lambda y: sorted(y.keys()))
-scan("192.168.0.104/24")
+
+    return client_list
+
+def print_result(result_list):
+    print(Fore.LIGHTBLUE_EX + "IP\t\t\tMAC Address\n----------------------------------------------------------")
+    for client in result_list:
+        print(client["ip"] + "\t\t" + client["mac"])
+
+def get_ip_range():
+    while True:
+        ip_range = input(Fore.MAGENTA + "╔═════$ Ingrese el rango de IP de la red que desea escanear (ej. 192.168.1.0/24): " + Fore.RESET)
+        if ip_range:
+            try:
+                network = ipaddress.ip_network(ip_range, strict=False)
+                return network
+            except ValueError:
+                print(Fore.LIGHTRED_EX + "═════╝ Rango de IP inválido." + Fore.RESET)
+        else:
+            print(Fore.LIGHTRED_EX + "═════╝ Debe ingresar un rango de IP válido." + Fore.RESET)
+
+def scan_network(network):
+    result = []
+    for ip in network.hosts():
+        result.extend(scan(str(ip)))
+    return result
+
+network = get_ip_range()
+result = scan_network(network)
+print_result(result)
